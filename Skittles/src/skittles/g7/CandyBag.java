67fb86d149a0fa3c348919256a23785ae4abd701
Neil Deshpande <neil.deshpa@gmail.com>
2011-11-07 15:32:10 -0500

    Changed CandyBag and added comments

--- Skittles/src/skittles/g7/CandyBag.java
+++ Skittles/src/skittles/g7/CandyBag.java
@@ -1,13 +1,23 @@
 package skittles.g7;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class CandyBag {
 	private Map<Integer, Candy> bag = new HashMap<Integer, Candy>();
+	
+	/**
+	 * Ordered in ascending order
+	 * 
+	 */
 	private SortedSet<Candy> orderedBag = new TreeSet<Candy>();
+	
 	
 	public CandyBag(int[] candies){
 		for(int i=0; i < candies.length; i++){
@@ -45,4 +55,47 @@ public class CandyBag {
 	public Candy getNthCandy(int n){
 		return orderedBag.toArray(new Candy[orderedBag.size()])[n];
 	}
+	
+	public List<Candy> sortByPreference(){
+		List<Candy> sortedCandies = new ArrayList<Candy>();
+		sortedCandies.addAll(bag.values());
+		Collections.sort(sortedCandies, new Comparator<Candy>(){
+
+			@Override
+			public int compare(Candy c1, Candy c2) {
+				return c1.getPref().compareTo(c2.getPref());
+			}
+			
+		});
+		
+		return sortedCandies;
+		
+	}
+	
+	
+	/**
+	 * 
+	 * Return the Candy that has a negative preference that's closest to zero
+	 * 
+	 * @return
+	 */
+	
+	public Candy getLeastNegative(){
+		// TODO: Complete this
+		
+		List<Candy> candies = sortByPreference();
+		return null;
+	}
+	
+	
+	/**
+	 * Return the Candy that will give you the least positive gain if you ate 
+	 * all of that candy
+	 * 
+	 */
+	public Candy getLeastPositive(){
+		// TODO: Complete this
+		return null;
+	}
+	
 }
